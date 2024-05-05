dec19deeb182fa12ed00e3ad461575ceddf69a69
EthanWTL <wangtianlong1207@gmail.com>
2024-05-05 02:00:36 -0500

    more animation for attack

--- Assets/Scripts/PlayerVFXManager.cs
+++ Assets/Scripts/PlayerVFXManager.cs
@@ -6,17 +6,38 @@ public class PlayerVFXManager : MonoBehaviour
 {
     public GameObject weapon;
 
+    public GameObject MeleeCombo1_spark;
+    public Transform MeleeCombo1_sparkTransform;
+
+    public GameObject MeleeCombo1_1st_slashSmoke;
+    public Transform MeleeCombo1_1st_slashSmokeTransForm;
+
     public GameObject MeleeCombo1_1st;
     public Transform MeleeCombo1_1stTransform;
 
     public GameObject MeleeCombo1_2nd;
     public Transform MeleeCombo1_2ndTransform;
 
+    public GameObject MeleeCombo1_2nd_slashSmoke;
+    public Transform MeleeCombo1_2nd_slashSmokeTransForm;
+
     public GameObject MeleeCombo1_3rd;
     public Transform MeleeCombo1_3rdTransform;
 
     public GameObject MeleeCombo1_3rd_slash;
     public Transform MeleeCombo1_3rd_slashTransform;
+
+    public GameObject MeleeCombo1_3rd_ground;
+    public Transform MeleeCombo1_3rd_groundTransform;
+
+
+
+    public void playMeleeCombo1_spark()
+    {
+        Vector3 localPosition = MeleeCombo1_sparkTransform.localPosition;
+        Vector3 worldPosition = weapon.transform.TransformPoint(localPosition);
+        GameObject combo1_1st = Instantiate(MeleeCombo1_spark, worldPosition, Quaternion.identity);
+    }
 
 
 
@@ -27,11 +48,25 @@ public class PlayerVFXManager : MonoBehaviour
         GameObject combo1_1st = Instantiate(MeleeCombo1_1st, worldPosition, MeleeCombo1_1stTransform.rotation);
     }
 
+    public void playMeleeCombo1_1st_slashSmoke()
+    {
+        Vector3 localPosition = MeleeCombo1_1st_slashSmokeTransForm.localPosition;
+        Vector3 worldPosition = weapon.transform.TransformPoint(localPosition);
+        GameObject combo1_1st = Instantiate(MeleeCombo1_1st_slashSmoke, worldPosition, MeleeCombo1_1st_slashSmokeTransForm.rotation);
+    }
+
     public void playMeleeCombo1_2nd()
     {
         Vector3 localPosition = MeleeCombo1_2ndTransform.localPosition;
         Vector3 worldPosition = weapon.transform.TransformPoint(localPosition);
         GameObject combo1_2nd = Instantiate(MeleeCombo1_2nd, worldPosition, MeleeCombo1_2ndTransform.rotation);
+    }
+
+    public void playMeleeCombo1_2nd_slashSmoke()
+    {
+        Vector3 localPosition = MeleeCombo1_2nd_slashSmokeTransForm.localPosition;
+        Vector3 worldPosition = weapon.transform.TransformPoint(localPosition);
+        GameObject combo1_1st = Instantiate(MeleeCombo1_2nd_slashSmoke, worldPosition, MeleeCombo1_2nd_slashSmokeTransForm.rotation);
     }
 
 
@@ -47,7 +82,14 @@ public class PlayerVFXManager : MonoBehaviour
     {
         Vector3 localPosition = MeleeCombo1_3rd_slashTransform.localPosition;
         Vector3 worldPosition = weapon.transform.TransformPoint(localPosition);
-        GameObject combo1_3rd = Instantiate(MeleeCombo1_3rd_slash, worldPosition, MeleeCombo1_3rd_slashTransform.rotation);
+        GameObject combo1_3rd_slash = Instantiate(MeleeCombo1_3rd_slash, worldPosition, MeleeCombo1_3rd_slashTransform.rotation);
+    }
+
+    public void playMeleeCombo1_3rd_gournd()
+    {
+        Vector3 localPosition = MeleeCombo1_3rd_groundTransform.localPosition;
+        Vector3 worldPosition = weapon.transform.TransformPoint(localPosition);
+        GameObject combo1_3rd_ground = Instantiate(MeleeCombo1_3rd_ground, worldPosition, Quaternion.identity);
     }
 
 
--- Assets/Scripts/WeaponGreatSword.cs
+++ Assets/Scripts/WeaponGreatSword.cs
@@ -7,11 +7,11 @@ public class WeaponGreatSword : MonoBehaviour
 {
 
     //player
-    public GameObject player;
+    private GameObject player;
     
     //animation
-    public PlayerCombatManager playerCombatManager;
-    public Animator playerAnimator;
+    private PlayerCombatManager playerCombatManager;
+    private Animator playerAnimator;
     public RuntimeAnimatorController runtimeAnimatorController;
 
     private string greatSwordAttack1 = "greatSword-attack01";
@@ -19,19 +19,31 @@ public class WeaponGreatSword : MonoBehaviour
     private string greatSwordAttack3 = "greatSword-attack03";
 
     //VFX
-    public PlayerVFXManager playerVFXManager;
+    private PlayerVFXManager playerVFXManager;
+
+    public GameObject combo1_spark;
+    public Transform MeleeCombo1_sparkTransform;
+
+    public GameObject combo1_1st_slashSmoke;
+    public Transform MeleeCombo1_1st_slashSmokeTransform;
 
     public GameObject combo1_1st;
     public Transform MeleeCombo1_1stTransform;
 
     public GameObject combo1_2nd;
     public Transform MeleeCombo1_2ndTransform;
-    
+
+    public GameObject combo1_2nd_slashSmoke;
+    public Transform MeleeCombo1_2nd_slashSmokeTransform;
+
     public GameObject combo1_3rd;
     public Transform MeleeCombo1_3rdTransform;
 
     public GameObject combo1_3rd_slash;
     public Transform MeleeCombo1_3rd_slashTransform;
+
+    public GameObject combo1_3rd_ground;
+    public Transform MeleeCombo1_3rd_groundTransform;
 
 
 
@@ -55,17 +67,29 @@ public class WeaponGreatSword : MonoBehaviour
         //assign VFX for animations
         playerVFXManager.weapon = gameObject;
 
+        playerVFXManager.MeleeCombo1_spark = combo1_spark;
+        playerVFXManager.MeleeCombo1_sparkTransform = MeleeCombo1_sparkTransform;
+
+        playerVFXManager.MeleeCombo1_1st_slashSmoke = combo1_1st_slashSmoke;
+        playerVFXManager.MeleeCombo1_1st_slashSmokeTransForm = MeleeCombo1_1st_slashSmokeTransform;
+
         playerVFXManager.MeleeCombo1_1st = combo1_1st;
         playerVFXManager.MeleeCombo1_1stTransform = MeleeCombo1_1stTransform;
         
         playerVFXManager.MeleeCombo1_2nd = combo1_2nd;
         playerVFXManager.MeleeCombo1_2ndTransform = MeleeCombo1_2ndTransform;
 
+        playerVFXManager.MeleeCombo1_2nd_slashSmoke = combo1_2nd_slashSmoke;
+        playerVFXManager.MeleeCombo1_2nd_slashSmokeTransForm = MeleeCombo1_2nd_slashSmokeTransform;
+
         playerVFXManager.MeleeCombo1_3rd = combo1_3rd;
         playerVFXManager.MeleeCombo1_3rdTransform = MeleeCombo1_3rdTransform;
 
         playerVFXManager.MeleeCombo1_3rd_slash = combo1_3rd_slash;
         playerVFXManager.MeleeCombo1_3rd_slashTransform = MeleeCombo1_3rd_slashTransform;
+
+        playerVFXManager.MeleeCombo1_3rd_ground = combo1_3rd_ground;
+        playerVFXManager.MeleeCombo1_3rd_groundTransform = MeleeCombo1_3rd_groundTransform;
     }
 
 
